aad17fafa3919acac03d52fc50c1e00740882162
thanyaresbenth <benthanyares@gmail.com>
2024-04-05 07:07:17 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import UserManagemnet from "./UserManagemnet";
 import UserList from "./UserList";
 import CreateNewUser from "./CreateNewUser";
+import './UserManagementForm.css';
+
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
        <Routes>
-         {/*<Route path="/" element={<UserManagemnet/>}>*/}
+           <Route path="/UserManagemnet" element={<UserManagemnet/>}></Route>
            <Route path="/user-list" element={<UserList/>}></Route>
            <Route path="/create-new-user" element={<CreateNewUser/>}></Route>
+
+
 
          {/*</Route>*/}
        </Routes>
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,73 +1,18 @@
 import {Button, Form, Input} from "antd";
 import Search from "antd/es/input/Search";
 import React, {useEffect, useState} from 'react';
-import { Space, Table, Tag } from 'antd';
+import { Space, Table } from 'antd';
 import axios from "axios";
 import moment from 'moment';
 import { Image } from 'antd';
-
-const columns = [
-    {
-        title: 'Profile picture',
-        dataIndex: 'image',
-        key: 'Image',
-        render :  (image, record, index) => {
-            return<>
-                <Image
-                    width={50}
-                    height={50}
-                    src="error"
-                    fallback={image}
-                />
-            </>;
-        }
-
-    },
-    {
-        title: 'First name',
-        dataIndex: 'first_name',
-        key: 'Firstname',
-    },
-    {
-        title: 'Last Name',
-        dataIndex: 'last_name',
-        key: 'Lastname',
-    },
-    {
-        title: 'Gender',
-        dataIndex: 'gender',
-        key: 'Gender',
-    },
-    {
-        title: 'Birthday',
-        dataIndex: 'birthday',
-        key: 'Birthday',
-        render :  (birthday, record, index) => {
-            let date = new Date(birthday);
-            let dateDisplay = moment(date).format('DD/MM/YYYY');
-
-            console.log(dateDisplay);
-            return <>{dateDisplay}</>;
-        }
+import {useNavigate} from "react-router-dom";
 
 
-    },
-    {
-        title: 'Action',
-        key: 'Action',
-        render: () => (
-            <Space size="middle">
-            <Button>Edit</Button>
-            <Button type="primary">Delete</Button>
-            </Space>
-
-        ),
-    }
-
-];
 const data = [
 
 ];
+
+
 
 // ...
 //เรียกApi
@@ -75,23 +20,91 @@ const API_URL = 'http://localhost:8080';
 
 function UserList() {
 
+    const columns = [
+        {
+            title: 'Profile picture',
+            dataIndex: 'image',
+            key: 'Image',
+            render :  (image, record, index) => {
+                return<>
+                    <Image
+                        width={50}
+                        height={50}
+                        src="error"
+                        fallback={image}
+                    />
+                </>;
+            }
+
+        },
+        {
+            title: 'First name',
+            dataIndex: 'first_name',
+            key: 'Firstname',
+        },
+        {
+            title: 'Last Name',
+            dataIndex: 'last_name',
+            key: 'Lastname',
+        },
+        {
+            title: 'Gender',
+            dataIndex: 'gender',
+            key: 'Gender',
+        },
+        {
+            title: 'Birthday',
+            dataIndex: 'birthday',
+            key: 'Birthday',
+            render :  (birthday, record, index) => {
+                let date = new Date(birthday);
+                let dateDisplay = moment(date).format('DD/MM/YYYY');
+
+                console.log(dateDisplay);
+                return <>{dateDisplay}</>;
+            }
+
+
+        },
+        {
+            title: 'Action',
+            key: 'Action',
+            render:  (text, record, index) => (
+
+                <Space size="middle">
+                    <Button onClick={() => edit(record.id)}> Edit</Button>
+                    <Button onClick={() => {deleteUser(record.id)}} type="primary">Delete</Button>
+                </Space>
+
+            ),
+        }
+
+    ];
+
     const [dataTable,setDataTable] = useState(data);
+    const navigate =useNavigate();
+
+    function edit(id){
+        console.log(id);
+        navigate(`/UserManagemnet?id=${id}`);
+    }
+    // edit function to navigate
+    function edit(id) {
+        console.log(id);
+        navigate(`/UserManagemnet?id=${id}`); // Navigate to the UserManagement component with query param
+    }
+
+// In your UserManagement component
+
+
+
+    function deleteUser(id){
+        console.log(id);
+    }
 
     const getUser = async () => {
         try {
             const response = await axios.get(`${API_URL}/users`);
-            console.log(response);
-            const userDatas = response.data;
-            console.log(`users `, userDatas);
-            setDataTable(userDatas);
-
-        } catch (errors) {
-            console.error(errors);
-        }
-    };
-    const putUser = async () => {
-        try {
-            const response = await axios.put(`${API_URL}/users/users/:id`);
             console.log(response);
             const userDatas = response.data;
             console.log(`users `, userDatas);
@@ -119,8 +132,7 @@ function UserList() {
             </Button>
 
         </div>
-         <Table columns={columns} dataSource={dataTable}   rowKey="id" />;
+        <Table columns={columns} dataSource={dataTable}   rowKey="id" />;
     </>;
-
 }
 export default UserList;
